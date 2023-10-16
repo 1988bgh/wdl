e2b85086efe72adb3283776b59b9d6cc87df902a
bae <1988_b@naver.com>
2023-10-16 22:36:18 +0900

    emailjs 추가

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 export const emailJsHandler = (() => {
   const formEl = document.querySelector(".contact__form");
-
+  const agreeCheck = document.querySelector("input[name='agree']");
   const userName = document.querySelector("input[name='user_name']");
   const userTeam = document.querySelector("input[name='user_team']");
   const userPhone = document.querySelector("input[name='user_phone']");
@@ -12,6 +12,42 @@ export const emailJsHandler = (() => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
 
+      if (userName.value == "") {
+        alert("성함을 입력해주세요.");
+        userName.focus();
+        return false;
+      }
+
+      if (userTeam.value == "") {
+        alert("소속을 입력해주세요.");
+        userName.focus();
+        return false;
+      }
+
+      if (userPhone.value == "") {
+        alert("전화번호를 입력해주세요.");
+        return false;
+      } else if (isNaN(userPhone.value)) {
+        alert("전화번호는 숫자만 입력가능합니다.");
+        return false;
+      }
+
+      if (userEmail.value == "") {
+        alert("이메일을 입력해주세요.");
+        return false;
+      } else {
+        const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+        if (pattern.test(userEmail.value) === false) {
+          alert("이메일 형식을 올바르게 작성해주세요.");
+          return false;
+        }
+      }
+
+      if (userMessage.value == "") {
+        alert("내용을 입력해주세요.");
+        return false;
+      }
+
       const templateParams = {
         name: userName.value,
         phone: userPhone.value,
@@ -20,13 +56,13 @@ export const emailJsHandler = (() => {
         message: userMessage.value,
       };
 
-      emailjs.send("service_k4u8o14", "template_78uvfmn", templateParams).then(
+      emailjs.send("service_u5ex3e7", "template_78uvfmn", templateParams).then(
         function (response) {
-          alert("문의주셔서 감사합니다. 빠른 회신 드리겠습니다.");
-          console.log("SUCCESS!", response.status, response.text);
+          alert("문의주셔서 감사합니다. 빠른 회신드리겠습니다.");
+          document.documentElement.scrollTop = 0;
         },
         function (error) {
-          console.log("FAILED...", error);
+          alert("전송에 실패했습니다. 다시 시도해 주세요.");
         }
       );
     });
